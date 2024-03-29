a25dad10b67a5192ce3b565e94d6c8dd61f3bf72
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2024-03-29 21:00:13 +0200

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { EventBindExampleComponent } from './components/event-bind-example/event-bind-example.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'event-bind-example', component: EventBindExampleComponent },
+  { path: 'welcome', component: WelcomeComponent },
+  { path: '', redirectTo: '/welcome', pathMatch: 'full' },
+];
